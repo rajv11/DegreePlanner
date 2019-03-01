0aa9c33c600b9dfa61bdb41ca67516fb47ee953c
Sai Charan Gurudu <s531499@nwmissouri.edu>
2019-03-01 12:44:58 -0600

    created Requirement and StudnetTerm. Updated Doc

---
+++ DegreePlanner/Models/Requirement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DegreePlanner.Models
+{
+    public class Requirement
+    {
+        public int RequirementID { get; set; }
+        public string RequirementAbbrev { get; set; }
+        public string RequirementName { get; set; }
+    }
+}
---
+++ DegreePlanner/Models/StudnetTerm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DegreePlanner.Models
+{
+    public class StudnetTerm
+    {
+        public int StudentTermID { get; set; }
+        public int StudentID { get; set; }
+        public int Term { get; set; }
+        public string TermAbbrev { get; set; }
+        public string TermLable { get; set; }
+
+    }
+}
