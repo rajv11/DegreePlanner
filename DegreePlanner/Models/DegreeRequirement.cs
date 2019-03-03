3b7b13c223296b23a39c9f3f92f82691cd721f6f
Yashwanth Reddy Bommineni <S531519@nwmissouri.edu>
2019-03-02 21:15:46 -0600

    added DegreeRequirement model

---
+++ DegreePlanner/Models/DegreeRequirement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DegreePlanner.Models
+{
+    public class DegreeRequirement
+    {
+        public int DegreeRequirementID { get; set; }
+        public int DegreeID { get; set; }
+        public int RequirementID { get; set; }
+    }
+}
